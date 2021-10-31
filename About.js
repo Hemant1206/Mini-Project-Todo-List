3eac22cd068b1ae29825cdc043996d3ab61f17fa
Hemant1206 <89117493+Hemant1206@users.noreply.github.com>
2021-10-31 09:35:36 +0530

    Add files via upload

---
+++ About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component
+            <p>Making Todo List as mini project.</p>
+        </div>
+    )
+}
